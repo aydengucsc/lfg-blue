b858c2dcd7fcc9b716733ec80fa56c6bd3e6ac9c
aydengucsc <aydeng@ucsc.edu>
2017-10-25 17:47:37 -0700

    lost files added

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var GameState = 
+{
+		preload: function()
+		{
+			this.load.image('background', 'assets/images/background.png');
+		},
+		create: function()
+		{
+			this.background = this.game.add.sprite(0,0,'background');
+		},
+		update: function()
+		{
+			
+		}
+};
+
+var game = new Phaser.Game(540, 960, Phaser.AUTO);
+game.state.add('GameState',GameState);
+game.state.start('GameState');

cefe850064facfa2aa9ef4ccc62ee44ce83ec150
jwang104 <jwang104@ucsc.edu>
2017-11-01 15:52:27 -0700

    pause buttons now go away properly

--- js/Level1.js
+++ js/Level1.js
@@ -10,30 +10,36 @@ var Level1 =
 		console.log("lv.1");
 		test = 0;
 		background = game.add.sprite(0, 0, 'background');
+		//temporary? pause button
+		//it's not killed when we pause, just covered. You can still click it to unpause.
+		//Intentional. This makes testing easier
 		this.createButton("Pause",game.world.centerX*1.65, 100, 300, 100, this.pauseFunct);
-		//var pauseButton = game.add.button(game.world.centerX*1.65, 100,'button',this.pauseFunct,this,2,1,0);
-		// pauseButton.anchor.setTo(0.5,0.5);
-		// pauseButton.width = 300;
-		// pauseButton.height = 100;
 	},
 	update: function()
 	{
+		//we don't actually have a game yet so here's something to pause
 		console.log("game up: ", test);
 		test++
 	},
+	//if paused, unpause and remove pause menu
 	pauseFunct: function(){
 		if(game.paused){
-			pauseScreen.destroy();
+			pauseScreen.kill();
+			resumeBtn[0].kill();
+			resumeBtn[1].kill();
+			menuBtn[0].kill();
+			menuBtn[1].kill();
 			game.paused = false;
-		}
+		}//if not paused, pause and make menu
 		else{
 		console.log("pause: ", test);
 		game.paused = true;
 		pauseScreen = game.add.sprite(0, 0, 'pause');
-		this.createButton("Resume",game.world.centerX,game.world.centerY+32, 300, 100, this.pauseFunct);
-		this.createButton("Menu",game.world.centerX,game.world.centerY+192, 300, 100, function(){game.state.start('MainMenu'); game.paused = false;});
+		resumeBtn = this.createButton("Resume",game.world.centerX,game.world.centerY+32, 300, 100, this.pauseFunct);
+		menuBtn = this.createButton("Menu",game.world.centerX,game.world.centerY+192, 300, 100, function(){game.state.start('MainMenu'); game.paused = false;});
 		}
 	},
+	//aydeng's createButton, modified to return the button&text so we can kill() them later.
 	createButton:function(string,x,y,w,h,callback)
 	{
 		var button1 = game.add.button(x,y,'button',callback,this,2,1,0);
@@ -42,6 +48,7 @@ var Level1 =
 		button1.height = h;
 		var txt = game.add.text(button1.x,button1.y,string,{font:"40px Arial", fill: "#0",align:"center"});
 		txt.anchor.setTo(0.5,0.5);
+		return {"0":button1,"1":txt};
 	}
 };
 // Level1.prototype =
